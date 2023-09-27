f9c955b50d2fe44f2349c8bc3a7b9e522c251450
siteflask1 <leandrolimahugo@gmail.com>
2023-09-27 04:58:56 -0300

    update

---
+++ .history/main_20230927044933.js
@@ -0,0 +1,10 @@
+import { renderizarCatalogo } from './src/cartaoProduto';
+import { atualizarPrecoCarrinho, inicializarCarrinho } from './src/menuCarrinho';
+
+
+
+
+renderizarCatalogo();
+inicializarCarrinho();
+
+atualizarPrecoCarrinho();
